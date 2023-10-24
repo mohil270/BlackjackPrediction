6a937f3db6d7adab086d4fb82a616f829b40a171
mohil270 <98039067+mohil270@users.noreply.github.com>
2023-10-24 14:38:08 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -7,6 +7,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import RandomizedSearchCV
+from flaml import AutoML
+
+import numpy as np
 
 DataComp1 = pd.read_csv('DataComp1.csv')
 
@@ -53,3 +57,54 @@ for name, model in models.items():
     # Calculate the accuracy of the model
     accuracy = accuracy_score(y_test, y_pred)
     print(f"{name} Accuracy: {accuracy * 100:.2f}%")
+
+
+param_grid = {
+    'learning_rate': np.linspace(0.01, 1, 20),
+    'max_depth': [3, 4, 5, 6, 7, 8, 9, 10],
+    'n_estimators': [50, 100, 150, 200, 250],
+    'subsample': np.linspace(0.5, 1, 10),
+    'colsample_bytree': np.linspace(0.5, 1, 10),
+    'gamma': np.linspace(0, 1, 10),
+    'reg_alpha': [0, 0.1, 0.5, 1],
+    'reg_lambda': [0.1, 0.5, 1, 2]
+}
+
+scale_pos_weight = len(y_train[y_train == 0]) / len(y_train[y_train == 1])
+xgb_model = XGBClassifier(use_label_encoder=False, eval_metric='logloss', scale_pos_weight=scale_pos_weight)
+rs = RandomizedSearchCV(xgb_model, param_grid, n_iter=100, scoring='accuracy', n_jobs=-1, cv=5, random_state=42)
+rs.fit(X_train, y_train)
+
+best_xgb = rs.best_estimator_
+
+y_pred = best_xgb.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Optimized XGBoost Accuracy: {accuracy * 100:.2f}%")
+
+
+
+from flaml import AutoML
+
+automl = AutoML()
+
+
+settings = {
+    "time_budget": 120, 
+    "metric": 'accuracy',
+    "task": 'classification',
+    "estimator_list": ['xgboost'], 
+    "log_file_name": 'flaml_log.log' 
+}
+
+automl.fit(X_train, y_train, **settings)
+
+print('Best ML leaner:', automl.best_estimator)
+print('Best hyperparmeter config:', automl.best_config)
+print(f'Best accuracy on validation data: {1-automl.best_loss:.4g}')
+
+y_pred = automl.predict(X_test)
+print('Predicted labels:', y_pred)
+print('True labels:', y_test)
+
+from sklearn.metrics import accuracy_score
+print('Accuracy:', accuracy_score(y_test, y_pred))
